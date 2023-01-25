a2723b11904a431f195a88822ba377447d9c6157
msdio <rlagofka1@naver.com>
2023-01-26 00:45:03 +0900

    add eslint and vscode settings

--- next-custom/pages/_app.tsx
+++ next-custom/pages/_app.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { Global } from '@emotion/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
