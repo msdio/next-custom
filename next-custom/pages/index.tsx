a868243f21903ba64cb19fc6f25d67ef1d6336c1
msdio <rlagofka1@gmail.com>
2023-01-26 02:19:39 +0900

    setting for storybook

--- next-custom/pages/index.tsx
+++ next-custom/pages/index.tsx
@@ -1,3 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
 export default function Home() {
-  return <h1>hello next!</h1>;
+  return (
+    <h1
+      css={css({
+        color: 'red',
+      })}
+    >
+      hello next!
+    </h1>
+  );
 }
